dc9775658fdec9cd807108192b2815b364d5dea8
solaryasha <solaryasha@gmail.com>
2020-06-15 16:26:51 +0300

    First attempt

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('hello mate academy!');
+const list = document.querySelector('.gallery__list');
+const largeImage = document.querySelector('.gallery__large-img');
+
+list.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const linkAddress = e.target.parentNode.href;
+
+  largeImage.src = linkAddress;
+});
